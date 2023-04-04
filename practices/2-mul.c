72554cbea80a3040e3252e1457db70f0160d8b9a
farahahmeddeen <farahahmeddeen1995@gmai.com>
2023-04-04 01:56:04 -0700

    random questions practices

---
+++ practices/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul - function that multiples two integers
+ *
+ * @a: parameter
+ * @b: parameter
+ *
+ * Return: value to be printed
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
--- practices/main.h
+++ practices/main.h
@@ -2,4 +2,5 @@
 #define main_h
 int _putchar(char c);
 int print_last_digit(int);
+int mul(int a, int b);
 #endif
