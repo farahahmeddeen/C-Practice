0df33adcf846646e49032b385fab1f40fe562e46
farahahmeddeen <farahahmeddeen1995@gmai.com>
2023-04-10 07:49:52 -0700

    function that prints sum of natural numbers

---
+++ learn_C/sumN.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int j;
+	int sum = 0;
+
+	printf("Enter a set of number: ");
+	scanf("%d", &i);
+
+	for (j = 1i; j <= i; j++)
+	{
+		sum = sum + j;
+	}
+	printf("sum is: %d", sum);
+	printf("\n");
+	return (0);
+}
